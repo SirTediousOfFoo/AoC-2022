2bce10a2b4802f247338955401e5fe47d0859a04
sirTedious <pero.milosev@yahoo.com>
2022-12-14 15:39:21 +0100

    Nice and deep indentation star 1 works

--- day5/src/main.rs
+++ day5/src/main.rs
@@ -1,12 +1,13 @@
 use std::{fs, vec};
 
 fn main() {
-    let binding = fs::read_to_string("test.input").expect("Unable to read file");
+    let binding = fs::read_to_string("puzzle.input").expect("Unable to read file");
     let lines = binding.lines();
 
     let mut vector = Vec::new();
     let mut spaces = 0;
     let mut vector2 = Vec::new();
+    let mut fin = Vec::new();
 
     for line in lines.clone() {
 
@@ -40,22 +41,48 @@ fn main() {
             t[i].push(r[i]);
         }
     }
+    
+    for i in 0..vector2[0].len() {
+        vector = [].to_vec();
+
+        for member in &vector2{
+            if member[i].ne(&' ') {
+                vector.push(member[i]);
+            } 
+        }
+        fin.push(vector);
+            vector = [].to_vec();
+    }
+    
     let mut v = vec![];
 
     for line in lines {
         if line.starts_with("m") {
-            v = line.split(" ").filter(|x| 
-            x.ne(&"move") && x.ne(&"from") && x.ne(&"to")).collect::<Vec<&str>>();
+            v = line.split(" ")
+                .filter(|x| 
+                    x.ne(&"move") && x.ne(&"from") && x.ne(&"to"))
+                .collect::<Vec<&str>>();
         }
 
         if v.len().ne(&0) {
-            
-            println!("{:?}",v);
+          
+            for i in 0..(v[0].to_string().parse::<i32>().unwrap()) {
+
+                let c = fin[v[1].to_string().parse::<usize>().unwrap()-1][0];
+
+                fin[v[2].to_string().parse::<usize>().unwrap()-1].insert(0, c);
+
+                fin[v[1].to_string().parse::<usize>().unwrap()-1].remove(0);
+            }
         }
             
     }
 
-    println!("{:?}",t);
+    let mut output = Vec::new();
 
-    println!("{:?}", vector2)
+    for thing in fin {
+        output.push(thing[0]);
+    }
+
+    println!("{:?}", output);
 }
